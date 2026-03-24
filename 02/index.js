778ed6e0477047f35f69d959b107e3d7bc1621ea
eldergsilva <eldergsilva@gmail.com>
2026-03-24 10:52:14 -0300

    exercicio 02 Carro

--- 02/index.js
+++ 02/index.js
@@ -1,7 +1,57 @@
 const carro = {
-    ligado : false,
-    velocidade:0,
-    function: ligar(),
-    function:desligar(),
-    function:acelerar() 
-}
+    ligado: false,
+    velocidade: 0,
+
+    exibirStatus: function () {
+        const status = this.ligado ? "ligado" : "desligado";
+        console.log(`Carro ${status}. Velocidade: ${this.velocidade}.`);
+    },
+
+    ligar: function () {
+        if (this.ligado) {
+            console.log("Este carro já está ligado.");
+        } else {
+            this.ligado = true;
+            this.exibirStatus();
+        }
+    },
+
+    desligar: function () {
+        if (!this.ligado) {
+            console.log("Este carro já está desligado.");
+        } else {
+            this.ligado = false;
+            this.velocidade = 0;
+            this.exibirStatus();
+        }
+    },
+
+    acelerar: function () {
+        if (!this.ligado) {
+            console.log("Não é possível acelerar um carro desligado.");
+        } else {
+            this.velocidade += 10;
+            this.exibirStatus();
+        }
+    },
+
+    desacelerar: function () {
+        if (!this.ligado) {
+            console.log("Não é possível desacelerar um carro desligado.");
+        } else {
+            this.velocidade -= 10;
+            this.exibirStatus();
+        }
+    }
+};
+
+// --- Sequência de execução (item f) ---
+carro.desligar();
+carro.ligar();
+carro.ligar();
+carro.acelerar();
+carro.acelerar();
+carro.desacelerar();
+carro.desligar();
+carro.acelerar();
+carro.desacelerar();
