25aab4e6809cb795a94c1a422d2b8a11b2d947e0
eldergsilva <eldergsilva@gmail.com>
2026-03-25 19:32:38 -0300

    exercicio 03 Carrinho de um e-commerce

--- 03/index.js
+++ 03/index.js
@@ -15,22 +15,7 @@ const carrinho = {
         }
     ],
 
-    imprimirResumo: function () {
-        let totaldeItens = 0;
-        let totalAPagar = 0;
-
-        for (const produto of this.produtos) {
-            totaldeItens += produto.qtd;
-            totalAPagar += (produto.precoUnit * produto.qtd) / 100;
-        }
-
-        console.log(`
-    Cliente: ${this.nomeDoCliente}
-    Total de itens: ${totaldeItens} itens
-    Total a pagar: R$ ${totalAPagar.toFixed(2)}`);
-    },
-
-    addProduto: function (produto) { 
+    addProduto: function (produto) {
         let itemEncontrado = null;
 
         for (const item of this.produtos) {
@@ -41,18 +26,57 @@ const carrinho = {
         }
 
         if (itemEncontrado) {
-            
             itemEncontrado.qtd += produto.qtd;
         } else {
-          
             this.produtos.push(produto);
         }
+    },
+
+    calcularTotalDeItens: function () {
+        let totalDeItens = 0;
+        for (const produto of this.produtos) {
+            totalDeItens += produto.qtd;
+        }
+        return totalDeItens;
+    },
+
+    calcularTotalAPagar: function () {
+        let totalAPagar = 0;
+        for (const produto of this.produtos) {
+            totalAPagar += (produto.precoUnit * produto.qtd);
+        }
+        return totalAPagar; // Retorna em centavos
+    },
+
+    imprimirResumo: function () {
+        const itens = this.calcularTotalDeItens();
+        const total = this.calcularTotalAPagar() / 100;
+
+        console.log(`
+    Cliente: ${this.nomeDoCliente}
+    Total de itens: ${itens} itens
+    Total a pagar: R$ ${total.toFixed(2)}`);
+    },
+
+    imprimirDetalhes: function () {
+        console.log(`Cliente: ${this.nomeDoCliente}\n`);
+
+         
+        for (let i = 0; i < this.produtos.length; i++) {
+            const produto = this.produtos[i];
+            const valorDoItem = (produto.precoUnit * produto.qtd) / 100;
+            console.log(`Item ${i + 1} - ${produto.nome} - ${produto.qtd} und - R$ ${valorDoItem.toFixed(2)}`);
+        }
+
+        const itens = this.calcularTotalDeItens();
+        const total = this.calcularTotalAPagar() / 100;
+
+        console.log(`\nTotal de itens: ${itens} itens`);
+        console.log(`Total a pagar: R$ ${total.toFixed(2)}`);
     }
-}; 
+};
 
  
-
-
 const novaBermuda = {
     id: 2,
     nome: "Bermuda",
@@ -60,18 +84,16 @@ const novaBermuda = {
     precoUnit: 5000
 };
 
- 
-
- 
 const novoTenis = {
     id: 3,
     nome: "Tenis",
     qtd: 1,
     precoUnit: 10000
-}
+};
 
-carrinho.addProduto(novaBermuda); 
+carrinho.addProduto(novaBermuda);
 carrinho.imprimirResumo();
 
 carrinho.addProduto(novoTenis);
 carrinho.imprimirResumo();
+carrinho.imprimirDetalhes();
