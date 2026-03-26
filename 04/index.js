59d07b778af5ef4e2b5c8d16f53e86809bd08a70
eldergsilva <eldergsilva@gmail.com>
2026-03-26 12:26:15 -0300

    exercicio 04 parte c

--- 04/index.js
+++ 04/index.js
@@ -2,5 +2,18 @@ const contaBancaria = {
     nome: "Maria",
     saldo: 0,
     historicos: [],
-    depositar:function (valor){}
-}
+    depositar:function (valor){
+     this.saldo +=valor
+    contaBancaria.historicos.push({tipo:"Depósito",valor})
+    console.log(`Deposito de R$${valor} realizado para o cliente: ${this.nome}`);
+    
+    const saldoEmReais = contaBancaria.saldo / 100;
+    console.log(`Saldo atual: R$ ${saldoEmReais.toFixed(2)}`);
+    } 
+    }
+
+    contaBancaria.depositar(1000)
+    contaBancaria.depositar(1150)
+console.log(    contaBancaria.historicos
+);
+    
