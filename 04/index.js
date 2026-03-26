8efac93fe44dc623b272452e23c2f9664ac1f9d3
eldergsilva <eldergsilva@gmail.com>
2026-03-26 19:47:58 -0300

    exercicio 04 parte e

--- 04/index.js
+++ 04/index.js
@@ -2,18 +2,36 @@ const contaBancaria = {
     nome: "Maria",
     saldo: 0,
     historicos: [],
-    depositar:function (valor){
-     this.saldo +=valor
-    contaBancaria.historicos.push({tipo:"Depósito",valor})
-    console.log(`Deposito de R$${valor} realizado para o cliente: ${this.nome}`);
-    
-    const saldoEmReais = contaBancaria.saldo / 100;
-    console.log(`Saldo atual: R$ ${saldoEmReais.toFixed(2)}`);
-    } 
-    }
+    depositar: function (valor) {
+        this.saldo += valor;
 
-    contaBancaria.depositar(1000)
-    contaBancaria.depositar(1150)
-console.log(    contaBancaria.historicos
-);
-    
+        this.historicos.push({
+            tipo: "Depósito",
+            valor: valor
+        });
+
+        const valorEmReais = valor / 100;
+        return `Deposito de R$${valorEmReais} realizado para o cliente: ${this.nome}`;
+    },
+    sacar: function (valor) {
+        if (valor > this.saldo) {
+            return `Saldo insuficiente para o saque de: ${this.nome}`;
+        }
+
+        this.saldo -= valor;
+
+        this.historicos.push({
+            tipo: "Saque",
+            valor: valor
+        });
+
+        const valorEmReais = valor / 100;
+        return `Saque de R$${valorEmReais} realizado para o cliente: ${this.nome}`;
+    }
+}
+
+// Testes
+console.log(contaBancaria.depositar(10000));  
+console.log(contaBancaria.sacar(5000));      
+console.log(contaBancaria.sacar(6000));      
+console.log(contaBancaria.historicos);       
